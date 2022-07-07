2b564fab1799d10b83a5da2f6e65e48d51abceec
Leon L. Robinson <leon.robinson@qa.com>
2022-07-07 13:31:49 +0100

    Tokimekied

--- bingo/createdb.py
+++ bingo/createdb.py
@@ -4,7 +4,7 @@ from application.models import Users
 db.drop_all()
 db.create_all()
 
-for i in [{"login":"bobr", "fname":"Bob","lname":"Robers"},{"login":"janeg", "fname":"Jane","lname":"Grandy"},{"login":"eriols", "fname":"Eriol","lname":"Saucepan"}]:
+for i in [{"login":"ShioriF", "fname":"Shiori","lname":"Fujisaki"},{"login":"MioK", "fname":"Mio","lname":"Kisaragi"},{"login":"YuinaH", "fname":"Yuina","lname":"Himoo"}]:
     useradd = Users(user_first_name=i["fname"],user_last_name=i["lname"],user_login_name=i["login"])
     db.session.add(useradd)
 db.session.commit()
