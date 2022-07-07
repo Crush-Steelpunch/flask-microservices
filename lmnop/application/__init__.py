d94c532d8701a0bcc248b1f8963d95b6fd626016
Leon L. Robinson <leon.robinson@qa.com>
2022-07-07 11:21:29 +0100

    saving this file would have helped about 4 hours ago

--- lmnop/application/__init__.py
+++ lmnop/application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///leon.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'b9a5f346-e34e-4a0b-9b2a-4e9c309c735e'
 
